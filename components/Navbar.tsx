a91d2ee989b3aa1b012bef0d159e7569393122df
Michael Omudho <mikemudho@gmail.com>
2023-12-10 21:58:29 +0300

    Cart Section Of Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,86 @@
-import Link from "next/link"
-import MaxWidthWrapper from "./MaxWidthWrapper"
-import { Icons } from "./Icons"
-import NavItems from "./NavItems"
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Icons } from "./Icons";
+import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
+import { Span } from "next/dist/trace";
+import Cart from "./Cart";
+
 
 const Navbar = () => {
-    return( 
-        <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
-            <header className="relative bg-white">
-                <MaxWidthWrapper>
-                    <div className="border-b border-gray-200">
-                        <div className="flex h-16 items-center">
-                            {/*TODO: Mobile nav */}
+  const user = null;
 
-                            <div className="ml-4 flex lg:ml-0">
-                                <Link href='/'>
-                                    <Icons.logo className='h-10 w-10' />
-                                </Link>
-                            </div>
-                            <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
-                                {/* nav items */}
+  return (
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-white">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              {/*TODO: Mobile nav */}
 
-                                <NavItems/>
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10" />
+                </Link>
+              </div>
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                {/* nav items */}
 
-                            </div>
-                        </div>
+                <NavItems />
+              </div>
+
+              <div className="ml-auto flex items-centre">
+                <div className="hidded lg:flex lg:flex-1 lg:items-center lg:justify-end space-x-6">
+                  {user ? null : (
+                    <Link
+                      href="/sign-in"
+                      className={buttonVariants({
+                        variant: "ghost",
+                      })}
+                    >
+                      Sign In
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+
+                  {user ? (
+                    <p></p>
+                  ) : (
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Create Account
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
+                      />
                     </div>
-                </MaxWidthWrapper>
-            </header>
-        </div>
-    )
-}
+                  )}
 
-export default Navbar
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "KES" | "USD" | "EUR" | "GBP" |"BDT",
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
+) {
+    const {currency = "KES", notation = "compact"} = options
+
+    const numericPrice = typeof price === "string" ? parseFloat(price) : price
+
+    return new Intl.NumberFormat("en-US",{
+      style: "currency",
+      currency,
+      notation,
+      maximumFractionDigits: 2
+    }).format(numericPrice)
+  }
+
