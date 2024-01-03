bdce756bb6e279f7f8c86310b6c084e02aacc989
Michael Omudho <mikemudho@gmail.com>
2024-01-03 21:46:46 +0300

    Products and product files

---
+++ collections/ProductFile.ts
@@ -0,0 +1,62 @@
+import { User } from "../payload-types";
+import { BeforeChangeHook } from "payload/dist/collections/config/types";
+import { Access, CollectionConfig } from "payload/types";
+
+
+const addUser:BeforeChangeHook = ({req,data}) => {
+    const user = req.user as User | null
+
+    return{...data,user:user?.id}
+}
+
+const yourOwnAndPurchased: Access = async({req}) => {
+    const user = req.user as User | null
+
+    if(user?.role === "Admin") return true
+    if(!user) return false
+
+    const {} = await req.payload.find({
+        collection: "products",
+        depth:0,
+        where:{
+            user:{
+                equals:user.id
+            }
+
+        }
+    }) 
+
+
+
+}
+
+export const ProductFiles: CollectionConfig = {
+    slug: "product_files",
+    admin:{
+        hidden:({user}) => user.role !== 'admin',
+    },
+    hooks:{
+        beforeChange:[addUser]
+    },
+    access:{
+        read: yourOwnAndPurchased,
+    },
+    upload:{
+        staticURL: "/product_files",
+        staticDir: "product_files",
+        mimeTypes: ["umage/*","font/*","application/postcript"]
+    },
+    fields:[
+    {
+        name:"user",
+        type:"relationship",
+        relationTo: "users",
+        admin:{
+            condition:()=>false,
+        },
+        hasMany:false,
+        required: true,
+
+    },
+],
+}
--- collections/Products.ts
+++ collections/Products.ts
@@ -1,4 +1,4 @@
-import { PRODUCT_CATEGORIES } from "@/config/Index";
+import { PRODUCT_CATEGORIES } from "../config/Index";
 import { CollectionConfig } from "payload/types";
 
 export const Products:CollectionConfig = {
@@ -43,7 +43,98 @@ export const Products:CollectionConfig = {
             name:"category",
             label: "Category",
             type: "select",
-            options: PRODUCT_CATEGORIES.map(({label,value})=>({label,value}))
-        }
+            options: PRODUCT_CATEGORIES.map(({label,value})=>({label,value})),
+            required:true,
+        },
+        {
+            name:"product_files",
+            label: "Product file(s)",
+            type: "relationship",
+            required:true,
+            relationTo:"product_files",
+            hasMany: false, 
+        },
+        {
+            name: "approvedForSale",
+            label:"Product Status",
+            type:"select",
+            defaultValue: 'pending',
+            access:{
+                create: ({req})=> req.user.role === "admin",
+                read: ({req})=> req.user.role === "admin",
+                update: ({req})=> req.user.role === "admin",
+            },
+            options:[
+                {
+                    label: "Pending verification",
+                    value: "pending",
+                },
+                {
+                    label: "Approved",
+                    value: "approved",
+                },
+                {
+                    label: "Denied",
+                    value: "denied",
+                },
+            ]
+
+        },
+        {
+            name: "priceId",
+            access:{
+                create: () => false,
+                read: () => false,
+                update: () => false,    
+            },
+            type: "text", 
+            admin: {
+                hidden:true,
+            }
+        },
+        {
+            name: "stripeId",
+            access:{
+                create: () => false,
+                read: () => false,
+                update: () => false,    
+            },
+            type: "text", 
+            admin: {
+                hidden:true,
+            }
+        },
+        {
+            name: "pesapalId",
+            access:{
+                create: () => false,
+                read: () => false,
+                update: () => false,    
+            },
+            type: "text", 
+            admin: {
+                hidden:true,
+            }
+        },
+        {
+            name:"images",
+            type:"array",
+            label: "Product Images",
+            minRows: 1,
+            maxRows: 4, 
+            required:true,
+            labels:{
+                singular: "Image",
+                plural: "Images"
+            },
+            fields:[
+                {
+                    name:"image",
+                    type:"upload",
+                    relationTo: "media",
+                    required:true,
+                }
+            ],
+        },
     ]
 }
