0f2221684badc1b12761c8b3ff0edebee8f74b9b
Michael Omudho <mikemudho@gmail.com>
2024-01-20 22:04:02 +0300

    Products page

--- collections/Users.ts
+++ collections/Users.ts
@@ -1,22 +1,66 @@
-import { CollectionConfig } from "payload/types";
+import { PrimaryActionEmailHtml } from "../components/emails/PrimaryActionEmail";
+import { Access, CollectionConfig } from "payload/types";
+
+const adminsAndUser: Access = ({req:{user}}) => {
+    if(user.role === "Admin") return true
+
+    return{
+        id:{
+            equals: user.id,
+        },
+    }
+}
 
 export const Users: CollectionConfig = {
     slug:"users",
     auth: {
         verify:{
             generateEmailHTML:({token}) =>{
-                return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'>Please Verify your email</a>`
+                return PrimaryActionEmailHtml({
+                    actionLabel: "verify your account",
+                    buttonText: "Verify Account",
+                    href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
+                })
             }
         }
     },
     access:{
-        read: () => true,
+        read: adminsAndUser,
         create:()=>true,
+        update:({req}) => req.user.role === "Admin",
+        delete:({req}) => req.user.role === "Admin",
+
+    },
+
+    admin:{
+        hidden:({user}) => user.role !=="Admin", 
+        defaultColumns:['id'],
     },
     fields:[
         {
+            name: "products",
+            label: "products",
+            admin:{
+                condition:() => false,
+            },
+            type:"relationship",
+            relationTo: "products",
+            hasMany: true,
+        },
+      
+        {
+            name: "product_files",
+            label: "product_files",
+            admin:{
+                condition:() => false,
+            },
+            type:"relationship",
+            relationTo: "product_files",
+            hasMany: true,
+        },
+        {
             name:'role',
-            defaultValue: 'admin',
+            defaultValue: 'Admin',
             required: true,
             admin:{
                 condition: () =>false,
--- payload-types.ts
+++ payload-types.ts
@@ -20,6 +20,8 @@ export interface Config {
 }
 export interface User {
   id: string;
+  products?: (string | Product)[] | null;
+  product_files?: (string | ProductFile)[] | null;
   role?: ('Admin' | 'User') | null;
   updatedAt: string;
   createdAt: string;
